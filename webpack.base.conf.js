59f9c0790dc926216b00e1dd0774cd2b5aeaf379
Kate Fenchenko <fev3@mail.ru>
2020-01-13 17:15:18 +0300

    image loading, paths, file loader

---
+++ webpack.base.conf.js
@@ -0,0 +1,106 @@
+const path =  require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyWebpackPlugin =  require('copy-webpack-plugin')
+
+const PATHS = {
+    src: path.join(__dirname,'./src'),
+    dist: path.join(__dirname,'./dist'),
+    assets: 'assets/'
+}
+
+module.exports = {
+
+    externals: {
+        paths: PATHS
+    },
+    mode: 'production',
+    entry: {
+      app: PATHS.src
+    },  
+    output: {
+        // filename: '[name].js',
+        // path: path.resolve(__dirname, 'dist')
+        filename:  `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({}), new TerserPlugin ({}),
+        ]
+      },
+    plugins: [
+        new HtmlPlugin({
+            hash:false,
+            filename: 'index.html',
+            template: `${PATHS.src}/index.html`
+        }),
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`
+
+        }),
+        new CopyWebpackPlugin([
+            {from: `${PATHS.src}/img`, to: `${PATHS.assets}img`},
+            {from: `${PATHS.src}/static`, to: ''}
+
+        ])
+    ],
+    resolve: {
+      extensions: ['.js','.ts']
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader',
+            MiniCssExtractPlugin.loader, 
+            {
+              loader:'css-loader',
+              options: {sourceMap:true}
+            },
+            {
+              loader:'postcss-loader',
+              options: {sourceMap:true}
+            }],
+          },
+          {
+            test: /\.less$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+          },
+          {
+            test: /\.scss$/i,
+            use: [
+              'style-loader',
+              MiniCssExtractPlugin.loader, 
+              {
+                loader:'css-loader',
+                options: {sourceMap:true}
+              },
+              {
+                loader:'postcss-loader',
+                options: {sourceMap:true}
+              },
+              {
+                loader:'sass-loader',
+                options: {sourceMap:true}
+              } ],
+          },
+          { test: /\.js|ts$/, exclude: /node_modules/, loader: "babel-loader" },
+          { 
+              test: /\.(png|jpg|gif|svg)$/, 
+              exclude: /node_modules/, 
+              loader: "file-loader",
+              options: {
+                  name: '[name].[ext]'
+              }
+             }
+        ],
+      },
+
+    
+
+}
---
+++ webpack.dev.conf.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  // DEV settings gonna be here
+  mode: 'development',
+  devtool:'cheap-module-eval-source-map',
+  devServer: {
+    // historyApiFallback: true,
+    // noInfo: true,
+    contentBase: baseWebpackConfig.externals.paths.dist,
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    port: 9000,
+  },
+  plugins: [
+      new webpack.SourceMapDevToolPlugin({
+         filename:'[file].map' 
+      })
+
+  ]
+  });
+
+// export devWebpackConfig
+module.exports = new Promise((resolve, reject) => {
+ resolve(devWebpackConfig)
+})
